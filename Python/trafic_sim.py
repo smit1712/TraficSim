b1e99e185f0e01783aab8438872ab7352043f74a
smit1712 <32259732+smit1712@users.noreply.github.com>
2020-04-21 19:34:57 +0200

    Add files via upload

--- Python/trafic_sim.py
+++ Python/trafic_sim.py
@@ -4,7 +4,79 @@ from random import seed
 from random import randint
 import asyncio
 import websockets
-import json   
+import json
+import threading
+
+    
+
+
+class traffic_object:
+    def __init__(self, A1, A2, A3, A4, AB1, AB2, B1, B2, B3, B4, B5, BB1, C1, C2, C3, D1, D2, D3, E1, E2, EV1, EV2, EV3, EV4, FF1, FF2, FV1, FV2, FV3, FV4, GF1, GF2, GV1, GV2, GV3, GV4):
+        self.A1 = A1
+        self.A2 = A2
+        self.A3 = A3
+        self.A4 = A4
+        self.AB1 = AB1
+        self.AB2 = AB2
+        self.B1 = B1
+        self.B2 = B2
+        self.B3 = B3
+        self.B4 = B4
+        self.B5 = B5
+        self.BB1 = BB1
+        self.C1 = C1
+        self.C2 = C2
+        self.C3 = C3
+        self.D1 = D1
+        self.D2 = D2
+        self.D3 = D3
+        self.E1 = E1
+        self.E2 = E2
+        self.EV1 = EV1
+        self.EV2 = EV2
+        self.EV3 = EV3
+        self.EV4 = EV4
+        self.FF1 = FF1
+        self.FF2 = FF2
+        self.FV1 = FV1
+        self.FV2 = FV2
+        self.FV3 = FV3
+        self.FV4 = FV4
+        self.GF1 = GF1
+        self.GF2 = GF2
+        self.GV1 = GV1
+        self.GV2 = GV2
+        self.GV3 = GV3
+        self.GV4 = GV4
+
+def websocket_init():
+    # uri = "ws://trafic.azurewebsites.net/controller"
+    uri = "ws://18df0816.ngrok.io"
+    ws = websockets.connect(uri)
+    print("WS CONNECTED ON: " + uri )
+    return ws
+
+def websocket_receive(websockets):
+    while(True):
+        data = websockets.recv()
+        print(f"< {data}")
+    websockets.close()
+    print("websocketclosed")
+    
+
+async def websocket_send():
+    data = {"A1": len(lights[0].cars), "A2": len(lights[1].cars), "A3": len(lights[2].cars), "A4": len(lights[3].cars), "AB1": 0, "AB2": 0,
+                "B1": len(lights[4].cars), "B2": len(lights[5].cars), "B3": len(lights[6].cars), "B4": 0, "B5":0, "BB1": 0,
+                "C1":  len(lights[7].cars), "C2": len(lights[8].cars), "C3": len(lights[9].cars), "D1": len(lights[10].cars), "D2": len(lights[11].cars), "D3": len(lights[12].cars),
+                "E1": 0, "E2": 0, "EV1": 0, "EV2": 0, "EV3": 0, "EV4": 0,
+                "FF1": 0, "FF2": 0, "FV1": 0, "FV2": 0, "FV3": 0, "FV4": 0,
+                "GF1": 0, "GF2": 0, "GV1": 0, "GV2": 0, "GV3": 0, "GV4": 0}
+    uri1 = "ws://trafic.azurewebsites.net/controller"
+    # json_dump = json.dumps(data)
+    # await main.ws.send(json_dump)  
+    # greeting = await main.ws.recv()
+
+    # print(f"< {greeting}")
 
 
 seed(1)
@@ -45,11 +117,12 @@ class car:
         self.y = traficlight.route[0][1]
         self.x = traficlight.route[0][0]
         self.driveIndex = 0
+        print("New car!")
     def drive(self):
         MaxD = 30
 
         for c in self.traficlight.cars:
-            if c != self:
+            if c != self :
                 if self.x in range(c.x - MaxD, c.x + MaxD) and self.y in range(c.y - MaxD, c.y + MaxD):
                     return
         if self.driveIndex == 1 and self.traficlight.status != "Green":
@@ -109,8 +182,12 @@ nextSpawn = datetime.now() + timedelta(0,1)
 nextGreen = datetime.now() + timedelta(0,3) 
 nextSend = datetime.now() + timedelta(0,3) 
 
+ws = websocket_init()
+websocketthread = threading.Thread(target=websocket_receive,args=ws)
+websocketthread.start()
 
 while not crashed:
+    now = datetime.now()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
@@ -129,22 +206,21 @@ while not crashed:
         c.drive()   
         Drawcar(c.x, c.y)
  
-    if(datetime.now() > nextSpawn):
+    if(now > nextSpawn):
         i = randint(0, len(lights) -1)
         cars.append(car(lights[i]))
-        nextSpawn = datetime.now() + timedelta(0,2)
+        nextSpawn = now + timedelta(seconds=3)
 
-    if(datetime.now() > nextGreen):
+    if(now > nextGreen):
         i = randint(0, len(lights) -1)
         for l in lights:
             l.status = "Red"
         lights[i].status = "Green"
         print(lights[i].name)
-        nextGreen = datetime.now() + timedelta(0,15)
-        
-    # if(datetime.now() > nextSend):
-    #     asyncio.get_event_loop().run_until_complete(websocket_send())     
-    #     nextSpawn = datetime.now() + timedelta(0,2)
+        nextGreen = now + timedelta(seconds=10)
+    # if(now > nextSend):
+        # asyncio.get_event_loop().run_until_complete(websocket_send())     
+        # nextSpawn = datetime.now() + timedelta(seconds=5)
 
 
     for l in lights:
@@ -156,6 +232,6 @@ while not crashed:
 
     pygame.display.update()
     clock.tick(120)
-
+ws.close()
 pygame.quit()
 quit()
